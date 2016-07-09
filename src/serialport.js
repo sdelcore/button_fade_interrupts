00e385f6addc59d3ba6730b7c6bc5750f98dea76
Spencer Delcore <sdelcore@uwaterloo.ca>
2016-07-09 16:21:53 -0400

    removed serial port nodeJS script

--- src/serialport.js
+++
@@ -1,8 +0,0 @@
-var SerialPort = require("serialport").SerialPort;
-var serialport = new SerialPort("COM3");
-serialport.on('open', function(){
-  console.log('Serial Port Opend');
-  serialport.on('data', function(data){
-      console.log(data[0]);
-  });
-});
